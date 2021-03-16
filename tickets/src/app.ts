750d49fce8082197e2b6e45b2bd05d2df7c78b56
kdhamric <kdhamric@gmail.com>
2021-03-16 11:44:05 -0500

    change to make it work without a ssl cert

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -20,9 +20,17 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: false
   })
 )
+
+//did not have a cert in prod, so changed this
+/*app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+)*/
 
 
 
